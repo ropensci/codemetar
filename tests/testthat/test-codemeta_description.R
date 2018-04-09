0cb07c92e991478fe66c73182567fbe3846de50d
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-04-09 16:18:52 +0200

    cf #63

--- tests/testthat/test-codemeta_description.R
+++ tests/testthat/test-codemeta_description.R
@@ -5,8 +5,26 @@ testthat::test_that("We can use a preset id", {
   codemeta_description(f, id = "https://doi.org/10.looks.like/doi")
 })
 
-testthat::test_that("We can parse plain Authors: & Maintainers: entries", {
-  f <- system.file("examples/DESCRIPTION_ex1.dcf", package = "codemetar")
+testthat::test_that("Error if no URL", {
+  f <- system.file("examples/DESCRIPTION_no_URL", package = "codemetar")
   expect_error(codemeta_description(f))
 })
 
+testthat::test_that("Warning if no BugReports", {
+  f <- system.file("examples/DESCRIPTION_Rforge", package = "codemetar")
+  cm <- codemeta_description(f)
+  expect_equal(cm$codeRepository, "http://surveillance.R-Forge.R-project.org/")
+})
+
+testthat::test_that("Works R-forge", {
+  f <- system.file("examples/DESCRIPTION_two_URLs", package = "codemetar")
+  cm <- codemeta_description(f)
+  expect_equal(cm$codeRepository, "https://github.com/ropensci/essurvey")
+})
+
+
+
+testthat::test_that("Choose GitHub", {
+  f <- system.file("examples/DESCRIPTION_ex1.dcf", package = "codemetar")
+  expect_error(codemeta_description(f))
+})
