46da104107ce9ca1515ab09f20aecc50637968dc
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-04-11 12:38:07 +0200

    start work on opinions cf #76

--- tests/testthat/test-codemeta_description.R
+++ tests/testthat/test-codemeta_description.R
@@ -5,16 +5,7 @@ testthat::test_that("We can use a preset id", {
   codemeta_description(f, id = "https://doi.org/10.looks.like/doi")
 })
 
-testthat::test_that("Error if no URL", {
-  f <- system.file("examples/DESCRIPTION_no_URL", package = "codemetar")
-  expect_error(codemeta_description(f))
-})
 
-testthat::test_that("Warning if no BugReports", {
-  f <- system.file("examples/DESCRIPTION_Rforge", package = "codemetar")
-  cm <- codemeta_description(f)
-  expect_equal(cm$codeRepository, "http://surveillance.R-Forge.R-project.org/")
-})
 
 testthat::test_that("Works R-forge", {
   f <- system.file("examples/DESCRIPTION_two_URLs", package = "codemetar")
