71d9bc9b2d0f03274125d5fb7639f0ce197ceba7
Maëlle Salmon <maelle@itsalocke.com>
2018-04-30 08:08:30 +0200

    memoise extract_badges cf #134

--- R/utils.R
+++ R/utils.R
@@ -93,34 +93,7 @@ check_urls <- function(urls){
   }
 }
 
-#' Extract all badges from Markdown file
-#'
-#' @param path Path to Markdown file
-#'
-#' @return A tibble with for each badge its text, link and link to
-#' its image.
-#' @export
-#'
-#' @examples
-extract_badges <- function(path){
-  txt <- readLines(path)
-  badges1 <- unlist(stringr::str_match_all(txt, "\\[!\\[\\]\\(.*?\\)\\]\\(.*?\\)"))
-  badges2 <- unlist(stringr::str_match_all(txt, "\\[!\\[.*?\\]\\(.*?\\)\\]\\(.*?\\)"))
-  badges <- c(badges1, badges2)
-  badges[!is.na(badges)]
 
-  unique(do.call(rbind,
-          lapply(badges, parse_badge)))
-}
-
-parse_badge <- function(badge){
-  text <- stringr::str_match(badge, "\\[!\\[(.*?)\\]")[,2]
-  link <- stringr::str_match(badge, "\\)\\]\\((.*?)\\)")[,2]
-  image_link <- stringr::str_match(badge, "\\]\\((.*?)\\)\\]")[,2]
-  tibble::tibble(text = text,
-                 link = link,
-                 image_link = image_link)
-}
 
 whether_provider_badge <- function(badges, provider_name){
   if(provider_name == "Comprehensive R Archive Network (CRAN)"){
