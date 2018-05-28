4936e7c3281a963eb969fb9c7f1f851e43dd6a3c
Maëlle Salmon <maelle@itsalocke.com>
2018-05-28 10:29:41 +0200

    add test for badge parsing in md created from Rmd cc @noamross

--- tests/testthat/test-extract_badges.R
+++ tests/testthat/test-extract_badges.R
@@ -9,4 +9,8 @@ test_that("we can parse md and html badges", {
   f <- system.file("examples/README_fakepackage2.md", package="codemetar")
   badges <- extract_badges(f)
   testthat::expect_equal(nrow(badges), 11)
+
+  f <- system.file("examples/README_fakepackage3.md", package="codemetar")
+  badges <- extract_badges(f)
+  testthat::expect_equal(nrow(badges), 5)
 })
