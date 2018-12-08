128e7582cd6e0af95ca7ab447f1929414376a0e7
hsonne <hauke.sonnenberg@gmx.de>
2018-12-08 08:40:35 +0100

    Use new function example_file() in crosswalk tests

--- tests/testthat/test-crosswalk.R
+++ tests/testthat/test-crosswalk.R
@@ -3,8 +3,9 @@ testthat::context("crosswalk.R")
 testthat::test_that("we can call crosswalk", {
 
   skip_on_os("windows")
-  f <- read_json(system.file("examples/github_format.json",
-                             package = "codemetar"))
+
+  f <- read_json(example_file("github_format.json"))
+
   a <- crosswalk(f, "GitHub")
   testthat::expect_is(a, "json")
   writeLines(a, "test.json")
@@ -12,7 +13,7 @@ testthat::test_that("we can call crosswalk", {
   testthat::expect_true(v)
   unlink("test.json")
 
-  f <- read_json(system.file("examples/package.json", package = "codemetar"))
+  f <- read_json(example_file("package.json"))
   a <- crosswalk(f, "NodeJS")
   testthat::expect_is(a, "json")
   writeLines(a, "nodejs.json")
@@ -24,13 +25,10 @@ testthat::test_that("we can call crosswalk", {
   b <- drop_context(a)
   add_context(b, getOption("codemeta_context", "http://purl.org/codemeta/2.0"))
 
-
   ## Test transforms between columns
-  f <- read_json(system.file("examples/github_format.json",
-                             package = "codemetar"))
+  f <- read_json(example_file("github_format.json"))
+
   crosswalk(f, "GitHub", "Zenodo")
 
-
   crosswalk_table(from = "GitHub", to = c("Zenodo", "Figshare"))
-
 })
