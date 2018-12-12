ca5f9caec8ce0383da95415910eb5d8386469627
hsonne <hauke.sonnenberg@gmx.de>
2018-12-12 16:43:10 +0100

    Clean guess_provider.R
    - Create and use helper functions to avoid duplication
      of code.

--- R/guess_provider.R
+++ R/guess_provider.R
@@ -1,48 +1,94 @@
-## cache avail packages
-.CRAN <- function(){
-  utils::available.packages(
-    utils::contrib.url("https://cran.rstudio.com", "source"))
+#
+# Helper functions below
+#
+
+# .CRAN ------------------------------------------------------------------------
+## cache available packages
+.CRAN <- function() {
+
+  available_source_packages("https://cran.rstudio.com")
 }
 
+# .BIOC ------------------------------------------------------------------------
+.BIOC <- function() {
+
+  available_source_packages("https://www.bioconductor.org/packages/release/bioc")
+}
+
+# CRAN -------------------------------------------------------------------------
 CRAN <- memoise::memoise(.CRAN)
 
-.BIOC <- function(){
-  utils::available.packages(
-    utils::contrib.url(
-      "https://www.bioconductor.org/packages/release/bioc",
-      "source"
-    )
-  )
-}
-
+# BIOC -------------------------------------------------------------------------
 BIOC <- memoise::memoise(.BIOC)
 
+# guess_provider ---------------------------------------------------------------
 guess_provider <- function(pkg) {
+
   if (is.null(pkg)) {
+
     return(NULL)
   }
-  ## Assumes a single provider
 
-  if (pkg %in% CRAN()[, "Package"]) {
-    list(
-      "@id" = "https://cran.r-project.org",
-      "@type" = "Organization",
-      "name" = "Comprehensive R Archive Network (CRAN)",
-      "url" = "https://cran.r-project.org"
+  ## Assumes a single provider
+  if (is_cran_package(pkg)) {
+
+    new_codemeta_organization(
+      url = "https://cran.r-project.org",
+      name = "Comprehensive R Archive Network (CRAN)"
     )
 
+  } else if (is_bioconductor_package(pkg)) {
 
-
-  } else if (pkg %in% BIOC()[, "Package"]) {
-    list(
-      "@id" = "https://www.bioconductor.org",
-      "@type" = "Organization",
-      "name" = "BioConductor",
-      "url" = "https://www.bioconductor.org"
+    new_codemeta_organization(
+      url = "https://www.bioconductor.org",
+      name = "BioConductor"
     )
 
   } else {
+
     NULL
   }
+}
 
+#
+# Helper functions can later be moved to utils.R
+#
+
+# available_source_packages ----------------------------------------------------
+available_source_packages <- function(url) {
+
+  utils::available.packages(utils::contrib.url(url, "source"))
+}
+
+# is_cran_package --------------------------------------------------------------
+is_cran_package <- function(pkg) {
+
+  is_in_package_info(pkg, CRAN())
+}
+
+# is_bioconductor_package ------------------------------------------------------
+is_bioconductor_package <- function(pkg) {
+
+  is_in_package_info(pkg, BIOC())
+}
+
+# is_in_package_info -----------------------------------------------------------
+#' @param pkg package name
+#' @param package_info data frame or matrix with column \code{Package}, eg.
+#'   as returned by \code{\link[utils]{available.packages}}
+#' @noRd
+is_in_package_info <- function(pkg, package_info) {
+
+  pkg %in% package_info[, "Package"]
+}
+
+# new_codemeta_organization ----------------------------------------------------
+new_codemeta_organization <- function(url, name) {
+
+  list(
+    "@id" = url,
+    "@type" = "Organization",
+    "name" = name,
+    "url" = url
+  )
 }
