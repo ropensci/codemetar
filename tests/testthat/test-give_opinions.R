16a4c4dc75656c279750bbf4d231dd964f3b6211
hsonne <hauke.sonnenberg@gmx.de>
2018-12-12 05:27:51 +0100

    Simplify tests using helper function and pipe operator
    - Name result variable always "hints" (short and clear)

--- tests/testthat/test-give_opinions.R
+++ tests/testthat/test-give_opinions.R
@@ -1,45 +1,49 @@
 context("test-give_opinions.R")
 
+# Define inline helper function to allow for short, clear tests
+contains <- function(hints, pattern) any(grepl(pattern, hints$fixme))
+
 testthat::test_that("Silent if ok URLs", {
-  f <- example_file("DESCRIPTION_good")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_null(desc_fixmes)
+  example_file("DESCRIPTION_good") %>%
+    give_opinions_desc() %>%
+    expect_null()
 })
 
 testthat::test_that("Message if no URL", {
-  f <- example_file("DESCRIPTION_no_URL")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_is(desc_fixmes, "data.frame")
-  expect_equal(desc_fixmes$where, "DESCRIPTION")
-  expect_true(any(grepl("URL", desc_fixmes$fixme)))
+  hints <- example_file("DESCRIPTION_no_URL") %>%
+    give_opinions_desc()
+  expect_is(hints, "data.frame")
+  expect_equal(hints$where, "DESCRIPTION")
+  expect_true(contains(hints, "URL"))
 })
 
 testthat::test_that("Message if no BugReports", {
-  f <- example_file("DESCRIPTION_no_bugreports")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_is(desc_fixmes, "data.frame")
-  expect_equal(desc_fixmes$where[1], "DESCRIPTION")
-  expect_true(any(grepl("BugReports", desc_fixmes$fixme)))
+  hints <- example_file("DESCRIPTION_no_bugreports") %>%
+    give_opinions_desc()
+  expect_is(hints, "data.frame")
+  expect_equal(hints$where[1], "DESCRIPTION")
+  expect_true(contains(hints, "BugReports"))
 })
 
 testthat::test_that("No message if ok description",{
-  f <- example_file("DESCRIPTION_Rforge")
-  expect_null(give_opinions_desc(f))
+  example_file("DESCRIPTION_Rforge") %>%
+    give_opinions_desc() %>%
+    expect_null()
 })
 
 test_that("Message if bad URLS", {
-  f <- example_file("DESCRIPTION_wrongURLS")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_true(any(grepl("Problematic URLs", desc_fixmes$fixme)))
+  hints <- example_file("DESCRIPTION_wrongURLS") %>%
+    give_opinions_desc()
+  expect_true(contains(hints, "Problematic URLs"))
 })
 
 test_that("badges in README", {
-  f <- example_file("README_codemetar_bad.md")
-  readme_fixmes <- give_opinions_readme(f, "codemetar")
-  expect_equal(nrow(readme_fixmes), 2)
-  expect_true(any(grepl("status", readme_fixmes$fixme)))
-  expect_true(any(grepl("CRAN", readme_fixmes$fixme)))
+  hints <- example_file("README_codemetar_bad.md") %>%
+    give_opinions_readme("codemetar")
+  expect_equal(nrow(hints), 2)
+  expect_true(contains(hints, "status"))
+  expect_true(contains(hints, "CRAN"))
 
-  readme_fixmes <- give_opinions_readme(f, "a4")
-  expect_true(any(grepl("BioConductor", readme_fixmes$fixme)))
+  hints <- give_opinions_readme(f, "a4")
+  expect_true(contains(hints, "BioConductor"))
 })
