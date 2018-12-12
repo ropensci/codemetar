ropensci/codemetar

commit 16a4c4dc75656c279750bbf4d231dd964f3b6211

Simplify tests using helper function and pipe operator
- Name result variable always "hints" (short and clear)

tests/testthat/test-give_opinions.R
@@ -1,45 +1,49 @@
 context("test-give_opinions.R")
 
+# Define inline helper function to allow for short, clear tests
+contains <- function(hints, pattern) any(grepl(pattern, hints$fixme))
+
 testthat::test_that("Silent if ok URLs", {
-  f <- example_file("DESCRIPTION_good")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_null(desc_fixmes)
+  example_file("DESCRIPTION_good") %>%
+    give_opinions_desc() %>%
+    expect_null()
 })
 
 testthat::test_that("Message if no URL", {
-  f <- example_file("DESCRIPTION_no_URL")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_is(desc_fixmes, "data.frame")
-  expect_equal(desc_fixmes$where, "DESCRIPTION")
-  expect_true(any(grepl("URL", desc_fixmes$fixme)))
+  hints <- example_file("DESCRIPTION_no_URL") %>%
+    give_opinions_desc()
+  expect_is(hints, "data.frame")
+  expect_equal(hints$where, "DESCRIPTION")
+  expect_true(contains(hints, "URL"))
 })
 
 testthat::test_that("Message if no BugReports", {
-  f <- example_file("DESCRIPTION_no_bugreports")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_is(desc_fixmes, "data.frame")
-  expect_equal(desc_fixmes$where[1], "DESCRIPTION")
-  expect_true(any(grepl("BugReports", desc_fixmes$fixme)))
+  hints <- example_file("DESCRIPTION_no_bugreports") %>%
+    give_opinions_desc()
+  expect_is(hints, "data.frame")
+  expect_equal(hints$where[1], "DESCRIPTION")
+  expect_true(contains(hints, "BugReports"))
 })
 
 testthat::test_that("No message if ok description",{
-  f <- example_file("DESCRIPTION_Rforge")
-  expect_null(give_opinions_desc(f))
+  example_file("DESCRIPTION_Rforge") %>%
+    give_opinions_desc() %>%
+    expect_null()
 })
 
 test_that("Message if bad URLS", {
-  f <- example_file("DESCRIPTION_wrongURLS")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_true(any(grepl("Problematic URLs", desc_fixmes$fixme)))
+  hints <- example_file("DESCRIPTION_wrongURLS") %>%
+    give_opinions_desc()
+  expect_true(contains(hints, "Problematic URLs"))
 })
 
 test_that("badges in README", {
-  f <- example_file("README_codemetar_bad.md")
-  readme_fixmes <- give_opinions_readme(f, "codemetar")
-  expect_equal(nrow(readme_fixmes), 2)
-  expect_true(any(grepl("status", readme_fixmes$fixme)))
-  expect_true(any(grepl("CRAN", readme_fixmes$fixme)))
+  hints <- example_file("README_codemetar_bad.md") %>%
+    give_opinions_readme("codemetar")
+  expect_equal(nrow(hints), 2)
+  expect_true(contains(hints, "status"))
+  expect_true(contains(hints, "CRAN"))
 
-  readme_fixmes <- give_opinions_readme(f, "a4")
-  expect_true(any(grepl("BioConductor", readme_fixmes$fixme)))
+  hints <- give_opinions_readme(f, "a4")
+  expect_true(contains(hints, "BioConductor"))
 })

commit 7e0968eab63c849f5f99706ba9c040ba382e4b3e

Refactor the functions in give_opinions.R  (#211)
* Add get_message() that returns a text given an id

* Refactor the functions in give_opinions.R

General:
- Get all messages from the function
  get_description(), see last commit.
- This function may be used by other parts of the
  package. Therefore, all message ids are prefixed
  "hint_" to indicate that they are used from the
  functions giving hints (opinions).
give_opinions():
- Rename "descr_path" to "description_file".
- Extract readme-related section to the new function
  try_to_give_opinions_readme(), just to keep a
  consistent level of abstraction.
- Collect different vectors of characters in the list
  "fixmes" and call rbind for all elements of this
  list.
- Return the data frame "fixmes". Then, there is no
  need for the last "else" since the data frame
  itself may be NULL.
try_to_give_opinions_readme() [new]
- Determine "pkg_path" from "description_file" (take
  its directory path).
- Instead of "else", return early if readme_path is
  NULL
read_description_if_null() [new]:
- Check if description is NULL. If yes, return this
  one else read the file.
give_opinions_desc():
- Pass description as additional argument so that
  desc::desc() does not need to be called multiple
  times.
- Call new function read_description_if_null() to
  check if the description is set and to read the
  description file otherwise, all in one row.
- Start with empty vector "fixmes" of character and
  directly add elements to this vector in the
  following. Thus, no intermediate variables
  (authors_fixme, url_fixme, bugreports_fixme) and no
  else branches are required.
- Use new helper function fails() that may later be
  moved to utils.R and can probably be used in other
  parts of the package.
- From the similar sections "URL" and "BugReports",
  create a new function add_url_fixmes().
- For putting the messages in a data frame, use new
  helper function fixmes_as_tibble_or_message() that
  can be called in one line and is reused in
  give_opinions_readme().
give_opinions_readme()
- Similar to give_opinions_desc().
- Use small helper has_provider_but_no_batch() to
  improve readability.
That's mainly it!

* Some say badge, some say batch

* Document get_message() but exclude from Rd files

* Move fails() to utils.R and add documentation

* Add tests for get_message()

* Move example_file() to utils.R and test it

* Use example_file() in tests, remove some line breaks

* Simplify tests using helper function and pipe operator

- Name result variable always "hints" (short and clear)

* Add tests for private functions

- Having difficulties to find useful test cases reveals
  that there is some room for improvement.

* Make test of example_file() less specific

- The test was failing on my computer.

* Fix bug in test of give_opinions_readme()

- The bug was introduced with my commit 16a4c4d

R/get_message.R
@@ -1,0 +1,54 @@
+# get_message ------------------------------------------------------------------
+#' Get Message String
+#'
+#' @param message_id keyword used to lookup a message string from the list
+#'   of messages defined in this function.
+#' @param \dots further arguments passed to \code{\link{sprintf}} that are used
+#'   to replace possible percent sign placeholders in the message string. As
+#'   many additional arguments as there are percent sign placeholders (so called
+#'   conversion specifiers, see \code{\link{sprintf}}) are required.
+#' @return message string defined for the given \code{keyword} as returned by
+#'   \code{\link{sprintf}} with possible placeholders replaced with the
+#'   formatted values given in \dots. If there is no message string defined for
+#'   the given keyword, an error is raised and a hint about available keywords
+#'   is given.
+#' @noRd
+get_message <- function(message_id, ...) {
+
+  messages <- list(
+
+    hint_use_authors_r = paste(
+      "The syntax Authors@R instead of plain Author and Maintainer is",
+      "recommended in particular because it allows richer metadata" # no lint
+    ),
+    hint_add_repo_url = paste(
+      "URL field. Indicate the URL to your code repository."
+    ),
+    hint_add_bug_report_url = paste(
+      "BugReports field. Indicate where to report bugs, e.g. GitHub issue",
+      "tracker."
+    ),
+    hint_add_status_badge = paste(
+      "Add a status badge cf e.g repostatus.org"
+    ),
+    hint_package_exists = paste(
+      "There is a package called %s on %s, add a badge to show it is yours",
+      "or rename your package."
+    ),
+    hint_highest_opinion = paste(
+      "codemetar has the highest opinion of this R package :-)"
+    )
+  )
+
+  if (message_id %in% names(messages)) {
+
+    sprintf(messages[[message_id]], ...)
+
+  } else {
+
+    stop(call. = FALSE, sprintf(
+      "No such message_id: %s.\nAvailable ids: %s",
+      message_id, paste(names(messages), collapse = ", ")
+    ))
+  }
+}

R/give_opinions.R
@@ -1,3 +1,5 @@
+# give_opinions ----------------------------------------------------------------
+
 #' Function giving opinions about a package
 #'
 #' @param pkg_path Path to the package root
@@ -5,123 +7,181 @@
 #' @return A data.frame of opinions
 #' @export
 #'
-give_opinions <- function(pkg_path = getwd()){
-  descr_path <- file.path(pkg_path, "DESCRIPTION")
-  # opinions about DESCRIPTION
-  descr_issues <- give_opinions_desc(descr_path)
+give_opinions <- function(pkg_path = getwd()) {
 
-  # opinions about README
-  readme <- guess_readme(pkg_path)$readme_path
-  if (is.null(readme)) {
-    readme_issues <- NULL
-  }else{
-    desc_info <- codemeta_description(descr_path)
-    readme_issues <- give_opinions_readme(readme,
-                                          desc_info$identifier)
+  # set the path to the description file
+  description_file <- file.path(pkg_path, "DESCRIPTION")
+
+  # read the description file
+  description <- read_description_if_null(NULL, description_file)
+
+  # create a list of issues, each element is a vector of character or NULL
+  fixmes <- do.call(rbind, list(
+
+    # opinions about DESCRIPTION
+    give_opinions_desc(description = description),
+
+    # opinions about README
+    try_to_give_opinions_readme(description_file)
+  ))
+
+  if (! is.null(fixmes)) {
+
+    fixmes$package <- as.character(description$get("Package"))
   }
 
-
-  fixmes <- rbind(descr_issues, readme_issues)
-
-  if(!is.null(fixmes)){
-    descr <- desc::desc(descr_path)
-    fixmes$package <- as.character(descr$get("Package"))
-    return(fixmes)
-  }else{
-    return(NULL)
-  }
-
-
+  fixmes
 }
 
-give_opinions_desc <- function(descr_path){
-  descr <- desc::desc(descr_path)
+# read_description_if_null -----------------------------------------------------
+
+#' Read Description from File If Not Given
+#'
+#' @param description object to be checked for \code{NULL} and to be returned if
+#'   not being \code{NULL}.
+#' @param description_file path to \code{DESCRIPTION} file to be read with
+#'   \code{\link[desc]{desc}} if \code{description} is \code{NULL}.
+#' @return \code{description} if not \code{NULL} or the content of
+#'   \code{description_file} read with \code{\link[desc]{desc}}.
+#' @noRd
+read_description_if_null <- function(description, description_file) {
+
+  if (is.null(description)) {
+
+    desc::desc(description_file)
+
+  } else {
+
+    description
+  }
+}
+
+# give_opinions_desc -----------------------------------------------------------
+
+#' Give Opinions about DESCRIPTION File
+#'
+#' You may either pass the path to a DESCRIPTION file or a description object
+#' as read with \code{\link[desc]{desc}}.
+#'
+#' @param description_file path do a DESCRIPTION file. Will not be considered if
+#'   a description object is given in \code{description}.
+#' @param description Description object as read with
+#'   \code{\link[desc]{desc}}. If not provided, the path to a DESCRIPTION file
+#'   must be given in \code{description_file}.
+#' @return \code{tibble} with columns \code{where}, \code{fixme} or \code{NULL}
+#'   if there are no opionions about the DESCRIPTION file.
+#' @noRd
+give_opinions_desc <- function(description_file, description = NULL) {
+
+  # read description from description_file if description is NULL
+  description <- read_description_if_null(description, description_file)
+
+  # Start with an empty vector of "fixme" messages
+  fixmes <- character()
+
   # Authors
-  if (inherits( try(descr$get_authors(), silent = TRUE),
-                'try-error')){
-    authors_fixme <- "The syntax Authors@R instead of plain Author and Maintainer is recommended in particular because it allows richer metadata" # no lint
-  }else{
-    authors_fixme <- NULL
+  if (fails(description$get_authors())) {
+
+    fixmes <- c(fixmes, get_message(message_id = "hint_use_authors_r"))
   }
 
   # URL
-  if(is.na(descr$get("URL"))){
-    url_fixme <- "URL field. Indicate the URL to your code repository."
-  }else{
-    checkurls <- check_urls(descr$descr$get_urls)
-    if(checkurls != ""){
-      url_fixme <- checkurls
-    }else{
-      url_fixme <- NULL
-    }
-
-  }
+  fixmes <- add_url_fixmes(
+    fixmes = fixmes,
+    main_url = description$get("URL"),
+    message_id = "hint_add_repo_url",
+    further_urls = description$descr$get_urls
+  )
 
   # BugReports
-  if(is.na(descr$get("BugReports"))){
-    bugreports_fixme <- "BugReports field. Indicate where to report bugs, e.g. GitHub issue tracker."
-  }else{
-    checkurls <- check_urls(descr$get("BugReports"))
-    if(checkurls != ""){
-      bugreports_fixme <- checkurls
-    }else{
-      bugreports_fixme <- NULL
-    }
+  fixmes <- add_url_fixmes(
+    fixmes = fixmes,
+    main_url = description$get("BugReports"),
+    message_id = "hint_add_bug_report_url"
+  )
 
-  }
-
-  fixmes <- c(authors_fixme, url_fixme, bugreports_fixme)
-  fixmes <- fixmes[!is.null(fixmes)]
-
-  if(length(fixmes) >0){
-
-    tibble::tibble(where = "DESCRIPTION",
-                   fixme = fixmes)
-  }else{
-    NULL
-  }
-
-
+  fixmes_as_tibble_or_message(fixmes, where = "DESCRIPTION")
 }
 
-give_opinions_readme <- function(readme_path,
-                                 pkg_name){
+# add_url_fixmes ---------------------------------------------------------------
+add_url_fixmes <- function(
+  fixmes, main_url, message_id, further_urls = main_url
+) {
 
-  # look for badges
-  badges <- extract_badges(readme_path)
+  if (is.na(main_url)) {
+
+    fixmes <- c(fixmes, get_message(message_id))
+
+  } else if ((failing_urls <- check_urls(further_urls)) != "") {
+
+    fixmes <- c(fixmes, failing_urls)
+  }
+
+  fixmes
+}
+
+# fixmes_as_tibble_or_message ---------------------------------------------------
+fixmes_as_tibble_or_message <- function(fixmes, where, message_id = NULL) {
+
+  if (length(fixmes)) {
+
+    tibble::tibble(where = where, fixme = fixmes)
+
+  } else if (! is.null(message_id)) {
+
+    message(get_message(message_id))
+  }
+}
+
+# try_to_give_opinions_readme --------------------------------------------------
+try_to_give_opinions_readme <- function(description_file) {
+
+  readme_path <- guess_readme(dirname(description_file))$readme_path
+
+  if (is.null(readme_path)) {
+
+    return(NULL)
+  }
+
+  desc_info <- codemeta_description(description_file)
+
+  give_opinions_readme(readme_path, pkg_name = desc_info$identifier)
+}
+
+# give_opinions_readme ---------------------------------------------------------
+give_opinions_readme <- function(readme_path, pkg_name) {
+
+  # start with an empty vector of "fixme" messages
+  fixmes <- character()
+
   # status
-  if(is.null(guess_devStatus(readme_path))){
-    status_fixme <- "Add a status badge cf e.g repostatus.org"
-  }else{
-    status_fixme <- NULL
+  if (is.null(guess_devStatus(readme_path))) {
+
+    fixmes <- c(fixmes, get_message("hint_add_status_badge"))
   }
 
   # provider
   provider <- guess_provider(pkg_name)
-  if(!is.null(provider)){
-    provider_badge <- whether_provider_badge(badges,
-                                             provider$name)
 
-    if(!provider_badge){
-      provider_fixme <- paste0("There is a package called ",
-                               pkg_name, " on ",
-                               provider$name, ", add a badge to show it is yours or rename your package.")
-    }else{
-      provider_fixme <- NULL
-    }
-  }else{
-    provider_fixme <- NULL
+  if (has_provider_but_no_badge(provider, readme_path)) {
+
+    fixmes <- c(fixmes, get_message(
+      "hint_package_exists", pkg_name, provider$name
+    ))
   }
 
-  fixmes <- c(status_fixme, provider_fixme)
-  fixmes <- fixmes[!is.null(fixmes)]
-  if(length(fixmes) >0){
+  fixmes_as_tibble_or_message(fixmes, "README", "hint_highest_opinion")
+}
 
-    tibble::tibble(where = "README",
-                   fixme = fixmes)
-  }else{
-    message("codemetar has the highest opinion of this R package :-)")
-    NULL
+# has_provider_but_no_badge ----------------------------------------------------
+has_provider_but_no_badge <- function(provider, readme_path) {
+
+  if (is.null(provider)) {
+
+    FALSE
+
+  } else {
+
+    ! whether_provider_badge(extract_badges(readme_path), provider$name)
   }
-
 }

R/utils.R
@@ -163,3 +163,22 @@ set_element_if_null <- function(x, element, value) {
 
   x
 }
+
+# fails ------------------------------------------------------------------------
+#' Does the Evaluation of an Expression Fail?
+#'
+#' @param expr expression to be evaluated within \code{try(\dots)}
+#' @param silent passed to \code{\link{try}}, see there.
+#' @return \code{TRUE} if evaluating \code{expr} failed and \code{FALSE} if
+#'   the evalutation of \code{expr} succeeded.
+#' @noRd
+fails <- function(expr, silent = TRUE) {
+
+  inherits(try(expr, silent = silent), "try-error")
+}
+
+# example_file -----------------------------------------------------------------
+example_file <- function(...) {
+
+  package_file("codemetar", "examples", ...)
+}

tests/testthat/test-codemeta_description.R
@@ -1,10 +1,5 @@
 testthat::context("codemeta_description.R")
 
-# Helper function to generate path to example file
-example_file <- function(...) {
-  package_file("codemetar", "examples", ...)
-}
-
 testthat::test_that("We can use a preset id", {
   f <- package_file("codemetar", "DESCRIPTION")
   codemeta_description(f, id = "https://doi.org/10.looks.like/doi")

tests/testthat/test-get_message.R
@@ -1,0 +1,10 @@
+testthat::context("get_message.R")
+
+test_that("get_message() works", {
+
+  expect_error(get_message(), "is missing, with no default")
+  expect_error(get_message("no_such_key"), "No such message_id")
+  expect_error(get_message("hint_package_exists"), "too few arguments")
+  expect_match(get_message("hint_package_exists", "abc", "def"), "abc on def")
+})
+

tests/testthat/test-give_opinions.R
@@ -1,48 +1,114 @@
 context("test-give_opinions.R")
+
+# Define inline helper function to allow for short, clear tests
+contains <- function(hints, pattern) any(grepl(pattern, hints$fixme))
+expect_missing_error <- function(x) expect_error(x, "missing")
+
 testthat::test_that("Silent if ok URLs", {
-  f <- system.file("examples/DESCRIPTION_good", package = "codemetar")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_null(desc_fixmes)
+  example_file("DESCRIPTION_good") %>%
+    give_opinions_desc() %>%
+    expect_null()
 })
 
-
 testthat::test_that("Message if no URL", {
-  f <- system.file("examples/DESCRIPTION_no_URL", package = "codemetar")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_is(desc_fixmes, "data.frame")
-  expect_equal(desc_fixmes$where, "DESCRIPTION")
-  expect_true(any(grepl("URL", desc_fixmes$fixme)))
+  hints <- example_file("DESCRIPTION_no_URL") %>%
+    give_opinions_desc()
+  expect_is(hints, "data.frame")
+  expect_equal(hints$where, "DESCRIPTION")
+  expect_true(contains(hints, "URL"))
 })
 
 testthat::test_that("Message if no BugReports", {
-  f <- system.file("examples/DESCRIPTION_no_bugreports", package = "codemetar")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_is(desc_fixmes, "data.frame")
-  expect_equal(desc_fixmes$where[1], "DESCRIPTION")
-  expect_true(any(grepl("BugReports", desc_fixmes$fixme)))
+  hints <- example_file("DESCRIPTION_no_bugreports") %>%
+    give_opinions_desc()
+  expect_is(hints, "data.frame")
+  expect_equal(hints$where[1], "DESCRIPTION")
+  expect_true(contains(hints, "BugReports"))
 })
 
 testthat::test_that("No message if ok description",{
-  f <- system.file("examples/DESCRIPTION_Rforge", package = "codemetar")
-  expect_null(give_opinions_desc(f))
+  example_file("DESCRIPTION_Rforge") %>%
+    give_opinions_desc() %>%
+    expect_null()
 })
 
 test_that("Message if bad URLS", {
-  f <- system.file("examples/DESCRIPTION_wrongURLS", package = "codemetar")
-  desc_fixmes <- give_opinions_desc(f)
-  expect_true(any(grepl("Problematic URLs", desc_fixmes$fixme)))
+  hints <- example_file("DESCRIPTION_wrongURLS") %>%
+    give_opinions_desc()
+  expect_true(contains(hints, "Problematic URLs"))
 })
 
 test_that("badges in README", {
-  f <- system.file("examples/README_codemetar_bad.md", package = "codemetar")
-  readme_fixmes <- give_opinions_readme(f, "codemetar")
-  expect_equal(nrow(readme_fixmes), 2)
-  expect_true(any(grepl("status",
-                        readme_fixmes$fixme)))
-  expect_true(any(grepl("CRAN",
-                        readme_fixmes$fixme)))
+  file <- example_file("README_codemetar_bad.md")
+  hints <- give_opinions_readme(file, "codemetar")
+  expect_equal(nrow(hints), 2)
+  expect_true(contains(hints, "status"))
+  expect_true(contains(hints, "CRAN"))
 
-  readme_fixmes <- give_opinions_readme(f, "a4")
-  expect_true(any(grepl("BioConductor",
-                        readme_fixmes$fixme)))
+  hints <- give_opinions_readme(file, "a4")
+  expect_true(contains(hints, "BioConductor"))
+})
+
+test_that("read_description_if_null() works", {
+
+  file <- example_file("DESCRIPTION_good")
+  description <- desc::desc(file)
+
+  result_1 <- read_description_if_null(description, "does_not_matter")
+  result_2 <- read_description_if_null(NULL, file)
+
+  expect_missing_error(read_description_if_null())
+  expect_identical(description, result_1)
+  expect_equal(description, result_2) # why not identical?
+})
+
+test_that("add_url_fixmes() works", {
+
+  expect_missing_error(add_url_fixmes())
+
+  result_1 <- add_url_fixmes("fixme", "no-such-url")
+  result_2 <- add_url_fixmes("fixme", "www.leo.org")
+  result_3 <- add_url_fixmes("fixmes", NA, "hint_add_repo_url")
+
+  expect_identical(result_1[1], "fixme")
+  expect_match(result_1[2], "No connection was possible")
+
+  expect_identical(result_2, "fixme")
+  expect_error(add_url_fixmes("fixmes", NA, "invalid_id"), "No such message_id")
+  expect_match(result_3[2], "Indicate the URL")
+})
+
+test_that("fixmes_as_tibble_or_message() works", {
+
+  expect_missing_error(fixmes_as_tibble_or_message())
+  expect_missing_error(fixmes_as_tibble_or_message("fix!"))
+
+  result <- fixmes_as_tibble_or_message("fix!", "my-package")
+  expect_is(result, "tbl_df")
+  expect_identical(dim(result), c(1L, 2L))
+  expect_identical(names(result), c("where", "fixme"))
+  expect_identical(result$where, "my-package")
+  expect_identical(result$fixme, "fix!")
+})
+
+test_that("try_to_give_opinions_readme() works", {
+
+  expect_missing_error(try_to_give_opinions_readme())
+  expect_null(try_to_give_opinions_readme(example_file("DESCRIPTION_good")))
+  expect_null(try_to_give_opinions_readme(package_file("dplyr", "DESCRIPTION")))
+})
+
+test_that("has_provider_but_no_badge() works", {
+
+  expect_missing_error(has_provider_but_no_badge())
+  expect_false(has_provider_but_no_badge(NULL))
+  expect_true(has_provider_but_no_badge(list(name = "my-provider")))
+  expect_missing_error(has_provider_but_no_badge(list(name = "BioConductor")))
+
+  file <- example_file("README_ex2.md")
+  expect_true(has_provider_but_no_badge(list(name = "BioConductor"), file))
+
+  file <- example_file("README_codemetar_good.md")
+  provider <- list(name = "Comprehensive R Archive Network (CRAN)")
+  expect_false(has_provider_but_no_badge(provider, file))
 })

tests/testthat/test-utils.R
@@ -10,3 +10,11 @@ testthat::test_that("get_root_path is covered", {
   ## test
 
 })
+
+testthat::test_that("example_file works", {
+
+  expect_match(example_file(), "/examples")
+  expect_match(example_file("abc"), "^$")
+  expect_true(file.exists(example_file("codemeta.json")))
+
+})
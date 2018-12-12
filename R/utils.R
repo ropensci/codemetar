7e0968eab63c849f5f99706ba9c040ba382e4b3e
Hauke Sonnenberg <hsonne@users.noreply.github.com>
2018-12-12 11:40:54 +0100

    Refactor the functions in give_opinions.R  (#211)
    * Add get_message() that returns a text given an id
    
    * Refactor the functions in give_opinions.R
    
    General:
    - Get all messages from the function
      get_description(), see last commit.
    - This function may be used by other parts of the
      package. Therefore, all message ids are prefixed
      "hint_" to indicate that they are used from the
      functions giving hints (opinions).
    give_opinions():
    - Rename "descr_path" to "description_file".
    - Extract readme-related section to the new function
      try_to_give_opinions_readme(), just to keep a
      consistent level of abstraction.
    - Collect different vectors of characters in the list
      "fixmes" and call rbind for all elements of this
      list.
    - Return the data frame "fixmes". Then, there is no
      need for the last "else" since the data frame
      itself may be NULL.
    try_to_give_opinions_readme() [new]
    - Determine "pkg_path" from "description_file" (take
      its directory path).
    - Instead of "else", return early if readme_path is
      NULL
    read_description_if_null() [new]:
    - Check if description is NULL. If yes, return this
      one else read the file.
    give_opinions_desc():
    - Pass description as additional argument so that
      desc::desc() does not need to be called multiple
      times.
    - Call new function read_description_if_null() to
      check if the description is set and to read the
      description file otherwise, all in one row.
    - Start with empty vector "fixmes" of character and
      directly add elements to this vector in the
      following. Thus, no intermediate variables
      (authors_fixme, url_fixme, bugreports_fixme) and no
      else branches are required.
    - Use new helper function fails() that may later be
      moved to utils.R and can probably be used in other
      parts of the package.
    - From the similar sections "URL" and "BugReports",
      create a new function add_url_fixmes().
    - For putting the messages in a data frame, use new
      helper function fixmes_as_tibble_or_message() that
      can be called in one line and is reused in
      give_opinions_readme().
    give_opinions_readme()
    - Similar to give_opinions_desc().
    - Use small helper has_provider_but_no_batch() to
      improve readability.
    That's mainly it!
    
    * Some say badge, some say batch
    
    * Document get_message() but exclude from Rd files
    
    * Move fails() to utils.R and add documentation
    
    * Add tests for get_message()
    
    * Move example_file() to utils.R and test it
    
    * Use example_file() in tests, remove some line breaks
    
    * Simplify tests using helper function and pipe operator
    
    - Name result variable always "hints" (short and clear)
    
    * Add tests for private functions
    
    - Having difficulties to find useful test cases reveals
      that there is some room for improvement.
    
    * Make test of example_file() less specific
    
    - The test was failing on my computer.
    
    * Fix bug in test of give_opinions_readme()
    
    - The bug was introduced with my commit 16a4c4d
    
    * Exclude private function fails() from documentation

--- R/utils.R
+++ R/utils.R
@@ -163,3 +163,22 @@ set_element_if_null <- function(x, element, value) {
 
   x
 }
+
+# fails ------------------------------------------------------------------------
+#' Does the Evaluation of an Expression Fail?
+#'
+#' @param expr expression to be evaluated within \code{try(\dots)}
+#' @param silent passed to \code{\link{try}}, see there.
+#' @return \code{TRUE} if evaluating \code{expr} failed and \code{FALSE} if
+#'   the evalutation of \code{expr} succeeded.
+#' @noRd
+fails <- function(expr, silent = TRUE) {
+
+  inherits(try(expr, silent = silent), "try-error")
+}
+
+# example_file -----------------------------------------------------------------
+example_file <- function(...) {
+
+  package_file("codemetar", "examples", ...)
+}
