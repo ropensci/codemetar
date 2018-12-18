cfa98b95d54492e1e944e18c36d616a4f6b46a65
Hauke Sonnenberg <hsonne@users.noreply.github.com>
2018-12-18 15:05:05 +0100

    Clean guess_other_metadata.R (#219)
    General
    - Call file.exists() and file.path() on a vector of
      files, store the result in "file_exists" and do
      checks on that vector
    - Combine the cases that return NULL
    
    guess_fileSize()
    - Put "silent" argument first in call to try() to
      allow for cleaner indentation
    - Do not use return() in the very last if-else

--- R/guess_other_metadata.R
+++ R/guess_other_metadata.R
@@ -1,47 +1,53 @@
-
+# guess_releaseNotes -----------------------------------------------------------
 guess_releaseNotes <- function(root = ".") {
-  ## First look for a local NEWS.md or NEWS, otherwise, give up
-  if (file.exists(file.path(root, "NEWS.md"))) {
-    releaseNotes <- "NEWS.md"
-  } else if (file.exists(file.path(root, "NEWS"))) {
-    releaseNotes <- "NEWS"
-  } else {
+
+  ## First look for a local NEWS.md or NEWS
+  news_files <- c("NEWS.md", "NEWS")
+
+  ## Do the news files exist?
+  file_exists <- file.exists(file.path(root, news_files))
+
+  ## Give up if there is no news file or if Git is not used
+  if (! any(file_exists) || ! uses_git(root)) {
+
     return(NULL)
   }
 
-  ## point to GitHub page
-  if (uses_git(root)) {
-    github_path(root, releaseNotes)
-  } else {
-    NULL
-  }
-
-
+  ## Point to the first file that was found: NEWS.md or NEWS on the GitHub page
+  github_path(root, news_files[file_exists][1])
 
   ## Consider pointing to CRAN NEWS, BIOC NEWS?
-
 }
 
+# guess_fileSize ---------------------------------------------------------------
 guess_fileSize <- function(root = ".") {
-  if (is.null(root)) {
-    return(NULL)
-  } else if (file.exists(file.path(root, "Meta"))) {
-    ## cannot build (or read file size?) on an already installed package
-    return(NULL)
-  } else if (!file.exists(file.path(root, "DESCRIPTION"))) {
-    return(NULL)
-  } else {
-    f <-
-      try(pkgbuild::build(root,
-                      vignettes = FALSE,
-                      manual = FALSE,
-                      quiet = TRUE),
-          silent = TRUE)
-    if(inherits(f, "try-error")){
-      return(NULL)
-    }else{
-      return(paste0(file.size(f) / 1e3, "KB"))
-    }
 
+  ## No root, no file size
+  if (is.null(root)) {
+
+    return(NULL)
+  }
+
+  ## Look for files 1. Meta, 2. DESCRIPTION
+  file_exists <- file.exists(file.path(root, c("Meta", "DESCRIPTION")))
+
+  ## Meta exists -> cannot build (or read file size?) on an already installed
+  ## package
+  if (file_exists[1] || ! file_exists[2]) {
+
+    return(NULL)
+  }
+
+  file_path <- try(silent = TRUE, pkgbuild::build(
+    root, vignettes = FALSE, manual = FALSE, quiet = TRUE
+  ))
+
+  if (inherits(file_path, "try-error")) {
+
+    NULL
+
+  } else {
+
+    paste0(file.size(file_path) / 1e3, "KB")
   }
 }
