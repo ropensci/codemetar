7fbfe68deff299517f0418eeebe8d6283c47a8d0
Carl Boettiger <cboettig@gmail.com>
2019-01-30 21:58:37 -0800

    tidy code for guess_github_metadata.R (#225)
    * Remove uses_git() that is also defined in utils.R
    
    - This version (using "ceiling = 0") is never used as
      the version in utils.R seems to take precedence
      (because "utils" comes after "guess_github_metadata"
      in alphabetical order?)
    - What is the desired behaviour, ceiling = 0 or the
      default (ceiling = NULL), or should ceiling be an
      argument to uses_git()?
    
    * Clean guess_github()
    
    - Return early with NULL if git is not used
    - Consequently use the pipe operator. It makes the code
      look clean but is there maybe any reason not to do so?
    
    * Clean github_path()
    
    - I think that there is no need to call
      git2r::repository() since it is already called
      within guess_github()
    - Return early
    - Use paste(..., sep = "/") instead of paste0()
    
    * Clean add_github_topics()
    
    - Use the pipe operator to avoid repetition of
      variable name "github"
    - Use getElment(1) instead of "[[1]]" to make this
      step a bit more obvious
    - no need for intermediate variables "owner" and
      "repo" since they are used only once in the call
       to gh::gh(). There, their meaning gets clear
       because the arguments are set by name.
    - Set silent = TRUE first in call to gh::gh(). This
      allows for a cleaner indentation.

--- R/guess_github_metadata.R
+++ R/guess_github_metadata.R
@@ -1,56 +1,69 @@
-# from devtools https://github.com/r-lib/devtools/blob/21fe55a912ca4eaa49ef5b7d891ff3e2aae7a370/R/git.R#L1
-# GPL>=2 code
-uses_git <- function(root) {
-  !is.null(git2r::discover_repository(root, ceiling = 0))
-}
-
-
+# remote_urls ------------------------------------------------------------------
 remote_urls <- function (r) {
+
   remotes <- git2r::remotes(r)
+
   stats::setNames(git2r::remote_url(r, remotes), remotes)
 }
 
+# guess_github -----------------------------------------------------------------
 guess_github <- function(root = ".") {
+
   ## from devtools
   # https://github.com/r-lib/devtools/blob/21fe55a912ca4eaa49ef5b7d891ff3e2aae7a370/R/git.R#L130
   # GPL>=2 code
 
-  if (uses_git(root)) {
-    r <- git2r::repository(root, discover = TRUE)
-    r_remote_urls <- grep("github", remote_urls(r), value = TRUE)
-    out <- r_remote_urls[[1]]
-    gsub("\\.git$", "", gsub("git@github.com:", "https://github.com/", out))
-  } else {
-    NULL
+  if (! uses_git(root)) {
+
+    return(NULL)
   }
+
+  root %>%
+    git2r::repository(discover = TRUE) %>%
+    remote_urls() %>%
+    grep(pattern = "github", value = TRUE) %>%
+    getElement(1) %>%
+    gsub(pattern = "git@github.com:", replacement = "https://github.com/") %>%
+    gsub(pattern = "\\.git$", replacement = "")
 }
+
+# github_path ------------------------------------------------------------------
 
 #' @importFrom git2r repository branches
 github_path <- function(root, path) {
-  base <- guess_github(root)
-  r <- git2r::repository(root, discover = TRUE)
-  branch <- getOption("codemeta_branch", "master")
-  paste0(base, "/blob/", branch, "/", path)
-}
 
+  if (is.null(base <- guess_github(root))) {
 
-# add GitHub topics
-add_github_topics <- function(cm){
-  github <- stringr::str_remove(cm$codeRepository, ".*github\\.com\\/")
-  github <- stringr::str_remove(github, "#.*")
-  github <- stringr::str_split(github, "/")[[1]]
-  owner <- github[1]
-  repo <- github[2]
-
-  topics <- try(gh::gh("GET /repos/:owner/:repo/topics",
-                   repo = repo, owner = owner,
-                   .send_headers = c(Accept = "application/vnd.github.mercy-preview+json")),
-                silent = TRUE)
-  if(!inherits(topics, "try-error")){
-    topics <- unlist(topics$names)
-
-    cm$keywords <- unique(c(cm$keywords, topics))
+    return(NULL)
   }
 
-  return(cm)
+  branch <- getOption("codemeta_branch", "master")
+
+  paste(base, "blob", branch, path, sep = "/")
+}
+
+# add_github_topics ------------------------------------------------------------
+add_github_topics <- function(codemeta) {
+
+  github <- codemeta$codeRepository %>%
+    stringr::str_remove(".*github\\.com\\/") %>%
+    stringr::str_remove("#.*") %>%
+    stringr::str_split("/") %>%
+    getElement(1)
+
+  topics <- try(silent = TRUE, gh::gh(
+    endpoint = "GET /repos/:owner/:repo/topics",
+    repo = github[2],
+    owner = github[1],
+    .send_headers = c(Accept = "application/vnd.github.mercy-preview+json")
+  ))
+
+  if (! inherits(topics, "try-error")) {
+
+    topics <- unlist(topics$names)
+
+    codemeta$keywords <- unique(c(codemeta$keywords, topics))
+  }
+
+  codemeta
 }
