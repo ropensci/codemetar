5edf3946d127aacf1927eb2221626a5a0e9c5607
Carl Boettiger <cboettig@gmail.com>
2019-04-21 12:06:42 -0700

    skip remote test on cran

--- tests/testthat/test-codemeta_validate.R
+++ tests/testthat/test-codemeta_validate.R
@@ -1,6 +1,7 @@
 testthat::context("validate")
 
 testthat::test_that("we can validate this file", {
+  skip_on_cran()
   write_codemeta("codemetar", "codemeta_test.json")
   testthat::expect_true(codemeta_validate("codemeta_test.json"))
   unlink("codemeta_test.json")
