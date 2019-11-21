4fa325d4789ce7f3f3f392f0d5d275ea5d4538e0
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-11-21 14:48:03 +0100

    a bit better

--- R/create_codemeta.R
+++ R/create_codemeta.R
@@ -15,19 +15,17 @@
 #' }
 #' @importFrom jsonlite read_json
 create_codemeta <- function(
-  pkg = ".",
-  root = ".",
-  id = NULL,
-  use_filesize = TRUE,
-  force_update =
-    getOption("codemeta_force_update", TRUE),
-  verbose = TRUE,
-  ...
-) {
+                            pkg = ".",
+                            root = ".",
+                            id = NULL,
+                            use_filesize = TRUE,
+                            force_update =
+                              getOption("codemeta_force_update", TRUE),
+                            verbose = TRUE,
+                            ...) {
 
   ## looks like we got a package name/path or Description file
   if (is.character(pkg)) {
-
     root <- get_root_path(pkg)
 
     # Set string constants
@@ -41,13 +39,11 @@ create_codemeta <- function(
 
       ## no cm, no existing codemeta.json found, start fresh
     } else {
-
       new_codemeta()
     }
 
     ## we got an existing codemeta object as pkg
   } else if (is.list(pkg)) {
-
     cm <- pkg
 
     ## root should be set already, we might check that root has a DESCRIPTION,
@@ -56,12 +52,10 @@ create_codemeta <- function(
   }
 
   if (verbose) {
-
     root <- get_root_path(pkg)
     opinions <- give_opinions(root)
 
-    if (! is.null(opinions)) {
-
+    if (!is.null(opinions)) {
       message(
         "Some elements could be improved, see our opinions via give_opinions('",
         root, "')"
@@ -75,27 +69,25 @@ create_codemeta <- function(
   ## Guess these only if not set in current codemeta
   # try to identify a code repo
 
-  if (! urltools::domain(cm$codeRepository) %in% source_code_domains()){
-    if (!is.null(guess_github(root)) && force_update) {
-      cm$relatedLink <- cm$codeRepository
-      cm$codeRepository <- guess_github(root)
+  if (!urltools::domain(cm$codeRepository) %in% source_code_domains()) {
+    if (uses_git(root)) {
+      if (!is.null(guess_github(root)) && force_update) {
+        cm$relatedLink <- cm$codeRepository
+        cm$codeRepository <- guess_github(root)
+      }
     }
   }
 
   if ((is.null(cm$releaseNotes) || force_update)) {
-
     cm$releaseNotes <- guess_releaseNotes(root)
   }
 
   if ((is.null(cm$readme) || force_update)) {
-
     cm$readme <- guess_readme(root)$readme_url
   }
 
   if (use_filesize) {
-
     if ((is.null(cm$fileSize) || force_update)) {
-
       cm$fileSize <- guess_fileSize(root)
     }
   }
@@ -103,20 +95,18 @@ create_codemeta <- function(
   # and if there's a readme
   readme <- guess_readme(root)$readme_path
 
-  if (! is.null(readme) && force_update) {
-
+  if (!is.null(readme) && force_update) {
     cm <- codemeta_readme(readme, codemeta = cm)
   }
 
   ## If code repo is GitHub
   if (urltools::domain(cm$codeRepository) %>%
-      github_domains()) {
-
+    github_domains()) {
     cm <- add_github_topics(cm)
   }
 
   ## Citation metadata
-  if (is.character(pkg)) {  ## Doesn't apply if pkg is a list (codemeta object)
+  if (is.character(pkg)) { ## Doesn't apply if pkg is a list (codemeta object)
 
     cm$citation <- guess_citation(pkg)
 
@@ -125,8 +115,7 @@ create_codemeta <- function(
 
     ## citations need schema.org context!
     ## see https://github.com/codemeta/codemeta/issues/155
-    if (! any(grepl(url_schema, cm$`@context`))) {
-
+    if (!any(grepl(url_schema, cm$`@context`))) {
       cm$`@context` <- c(cm$`@context`, url_schema)
     }
   }
@@ -137,27 +126,22 @@ create_codemeta <- function(
 
   provider <- guess_provider(cm$identifier)
 
-  if (! is.null(provider)) {
-
+  if (!is.null(provider)) {
     readme <- guess_readme(root)$readme_path
 
-    if (! is.null(readme)) {
-
+    if (!is.null(readme)) {
       badges <- extract_badges(readme)
 
-      if (! is.null(provider) &&
-          whether_provider_badge(badges, provider$name)) {
-
+      if (!is.null(provider) &&
+        whether_provider_badge(badges, provider$name)) {
         cm <- set_relatedLink_1(cm, provider)
       }
     } else if (cm$identifier %in% installed_package_names()) {
-
       pkg_info <- sessioninfo::package_info(cm$identifier)
       pkg_info <- pkg_info[pkg_info$package == cm$identifier, ]
       provider_name <- pkg_info$source
 
       if (cm$version == pkg_info$ondiskversion) {
-
         cm <- set_relatedLink_2(cm, provider_name)
       }
     }
@@ -169,15 +153,11 @@ create_codemeta <- function(
 
 # set_relatedLink_1 ------------------------------------------------------------
 set_relatedLink_1 <- function(codemeta, provider) {
-
   if (provider$name == "Comprehensive R Archive Network (CRAN)") {
-
     codemeta$relatedLink <- unique(c(
       codemeta$relatedLink, get_url_cran_package(codemeta$identifier)
     ))
-
   } else if (provider$name == "BioConductor") {
-
     codemeta$relatedLink <- unique(c(
       codemeta$relatedLink, get_url_bioconductor_package(codemeta$identifier)
     ))
@@ -188,24 +168,18 @@ set_relatedLink_1 <- function(codemeta, provider) {
 
 # set_relatedLink_2 ------------------------------------------------------------
 set_relatedLink_2 <- function(codemeta, provider_name) {
-
   if (grepl("CRAN", provider_name)) {
-
     codemeta$relatedLink <- unique(c(
       codemeta$relatedLink, get_url_cran_package(codemeta$identifier)
     ))
-
   } else if (grepl("Bioconductor", provider_name)) {
-
     codemeta$relatedLink <- unique(c(
       codemeta$relatedLink, get_url_bioconductor_package(codemeta$identifier)
     ))
-
   } else if (grepl("Github", provider_name)) {
 
     # if GitHub try to build the URL to commit or to repo in general
     if (grepl("@", provider_name)) {
-
       codemeta$relatedLink <- unique(c(
         codemeta$relatedLink, get_url_github_package(provider_name)
       ))
