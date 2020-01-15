d1802937200fb77cc9e46a8dae006dea850d2689
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-01-15 18:49:08 +0100

    better

--- R/create_codemeta.R
+++ R/create_codemeta.R
@@ -18,7 +18,7 @@ create_codemeta <- function(
   pkg = ".",
   root = ".",
   id = NULL,
-  use_filesize = TRUE,
+  use_filesize = FALSE,
   force_update =
     getOption("codemeta_force_update", TRUE),
   verbose = TRUE,
