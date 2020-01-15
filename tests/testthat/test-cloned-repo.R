7f664d8de5de93f40db695e5f884f65fc23c10ee
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-01-15 15:46:29 +0100

    rm empty tests

--- tests/testthat/test-cloned-repo.R
+++ tests/testthat/test-cloned-repo.R
@@ -24,12 +24,6 @@ testthat::test_that("we can generate codemeta
 
       })
 
-      guess_ci(file.path(f, "README.md"))
-      guess_devStatus(file.path(f, "README.md"))
-
-      guess_readme(f)
-      guess_releaseNotes(f)
-      guess_fileSize(f)
       file.remove(dir()[grepl(".tar.gz", dir())])
 
       file.remove("test.json")
@@ -48,12 +42,6 @@ testthat::test_that("we can generate codemeta
 
       })
 
-      guess_ci(file.path(f, "README.md"))
-      guess_devStatus(file.path(f, "README.md"))
-
-      guess_readme(f)
-      guess_releaseNotes(f)
-      guess_fileSize(f)
       unlink("codemetar-*.tar.gz")
 
       unlink("test.json")
@@ -64,8 +52,3 @@ testthat::test_that("we can generate codemeta
 
 
   })
-
-
-testthat::test_that("parse citation from source repo", {
-  a <- guess_citation(".")
-})
