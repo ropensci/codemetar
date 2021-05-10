99be2a2cf942043b89ce2bfe5c8d253d5dcff7f4
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-05-10 11:16:41 +0200

    1 test

--- tests/testthat/test-write_codemeta.R
+++ tests/testthat/test-write_codemeta.R
@@ -3,26 +3,9 @@ testthat::context("write_codemeta")
 testthat::test_that("we can create a codemeta document given a package name", {
   skip_on_cran()
   skip_if_offline()
-  testthat::expect_snapshot(
-    create_codemeta("utils", path = path, verbose = FALSE)
-  )
+  dir <- withr::local_tempdir()
+  usethis::create_package(dir, open = FALSE)
+  write_codemeta(dir)
+  expect_snapshot_file(file.path(dir, "codemeta.json"))
 
-})
-
-testthat::test_that("We can deduce relatedLink from installed pkg", {
-  skip_on_cran()
-  skip_if_offline()
-
-  xml2_cm <- create_codemeta(find.package("xml2"), verbose = FALSE)
-  testthat::expect_true("https://xml2.r-lib.org/" %in%
-                          xml2_cm$relatedLink)
-
-})
-
-## Test that we can write codemeta from a temp working dir (e.g. non-root dir)
-testthat::test_that("we can write codemeta given a codemeta object", {
-  skip_on_cran()
-  skip_if_offline()
-  codemeta <- new_codemeta()
-  expect_is(create_codemeta("codemetar", codemeta, verbose = FALSE), "list")
 })
